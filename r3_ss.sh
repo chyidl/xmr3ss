ce773d07031c92c5532943cac36d850315ca5a2a
chyidl <44197301+chyidl@users.noreply.github.com>
2019-04-15 23:27:40 +0800

    Update r3_ss.sh

--- r3_ss.sh
+++ r3_ss.sh
@@ -12,7 +12,7 @@ if [[ $EUID -ne 0 ]]; then
 fi
 cd /tmp
 rm -f shadowsocks_r3.tar.gz
-wget https://raw.githubusercontent.com/Chyi341152/xmr3ss/master/shadowsocks_r3.tar.gz
+wget https://raw.githubusercontent.com/chyidl/xmr3ss/master/shadowsocks_r3.tar.gz
 tar zxf shadowsocks_r3.tar.gz
 
 # install shadowsocks ss-redir to /data/usr/sbin
